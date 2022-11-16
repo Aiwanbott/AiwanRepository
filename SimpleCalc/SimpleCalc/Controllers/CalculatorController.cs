86c6e0b5c94b4b20aa058e88be93c9fad600a9b0
Ivan Siotko <vsiotko@gmail.com>
2022-11-16 17:31:29 +0200

    CALC

---
+++ SimpleCalc/SimpleCalc/Controllers/CalculatorController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SimpleCalc.Models;
+
+namespace SimpleCalc.Controllers
+{
+    [Route("[controller]")]
+    public class CalculatorController : Controller
+    {
+        private readonly ILogger<CalculatorController> _logger;
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost("addition")]
+        public IActionResult Add(Calculator cal)
+        {
+            double a, b;
+            a = cal.value1;
+            b = cal.value2;
+            return Ok(a + b);
+
+        }
+
+        [HttpPost("difference")]
+        public IActionResult Dif(Calculator cal)
+        {
+            double a, b;
+            a = cal.value1;
+            b = cal.value2;
+            return Ok(a - b);
+        }
+        [HttpPost("multiplication")]
+        public IActionResult Mul(Calculator cal)
+        {
+            double a,b;
+            a = cal.value1;
+            b = cal.value2;
+            return Ok(a * b);
+        }
+        [HttpPost("division")]
+        public IActionResult Div(Calculator cal)
+        {
+            double a, b;
+            a = cal.value1;
+            b = cal.value2;
+            return Ok(a / b);
+        }
+        
+        
+    }
+}
---
+++ SimpleCalc/SimpleCalc/Models/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCalc.Models
+{
+    public class Calculator
+    {
+        [Required]
+        public double value1 { get; set; }
+
+        [Required]
+        public double value2 { get; set; }
+    }
+}
